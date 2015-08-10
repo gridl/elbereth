7446b20f4535abd9d6e25a166526a830c6099e7e
Nicholas H.Tollervey <ntoll@ntoll.org>
2015-08-10 14:26:21 +0100

    Code tidy

--- elbereth/views.py
+++ elbereth/views.py
@@ -3,7 +3,7 @@ import json
 import random
 
 
-poem = '''A Elbereth Gilthoniel
+PHRASES = '''A Elbereth Gilthoniel
 silivren penna miriel
 o menel aglar elenath!
 Na-chaered palan-diriel
@@ -14,13 +14,13 @@ nef aear, si nef aearon!
 A Elbereth Gilthoniel
 o menel palan-diriel,
 le nallon si di'nguruthos!
-A tiro nin, Fanuilos!
-'''
+A tiro nin, Fanuilos!'''.split('\n')
 
 
 def gibberish(*args, **kwargs):
     response = json.dumps({
         'version': 0.1,
-        'elvish': random.choice(poem.split('\n'))
+        'elvish': random.choice(PHRASES),
+        'should_be_taken_seriously': False
     })
     return HttpResponse(response, content_type='application/json')
